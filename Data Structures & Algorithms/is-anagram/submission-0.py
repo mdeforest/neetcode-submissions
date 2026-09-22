d32a633cb0898af7932f90ea736d7113121597f1
Michaela DeForest <mdeforest@galileosuite.com>
2026-09-22 13:48:00 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        valuesTable = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
+        
+        for i in range(len(s)):
+            valuesTable[s[i]] += 1
+            valuesTable[t[i]] -= 1
+
+        for _, v in valuesTable.items():
+            if  v != 0:
+                return False
+        
+        return True
+        
